c854b3e677cb8fa2aef6363ed869cc5f4b844cf2
moeabdol <mohd.a.saed@gmail.com>
2017-07-27 16:07:59 +0300

    Demo API and app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'app';
+  apiRoot: string = "http://httpbin.org";
+
+  constructor(private http: HttpClient) { }
+
+  doGET() {
+    console.log("GET");
+  }
+
+  doPOST() {
+    console.log("POST");
+  }
+
+  doPUT() {
+    console.log("PUT");
+  }
+
+  doDELETE() {
+    console.log("DELETE");
+  }
+
+  doGETAsPromise() {
+    console.log("GET AS PROMISE");
+  }
+
+  doGETAsPromiseError() {
+    console.log("GET AS PROMISE ERROR");
+  }
+
+  doGETAsObservableError() {
+    console.log("GET AS OBSERVABLE ERROR");
+  }
+
+  doGETWithHeaders() {
+    console.log("GET WITH HEADERS");
+  }
 }
